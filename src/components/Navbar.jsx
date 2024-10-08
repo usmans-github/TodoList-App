552dd366cdc5d09e9c6f6b24000468de0282fe26
M Usman <usmanali730771@gmail.com>
2024-10-08 14:55:55 +0500

    Y

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
        
         <ul className="flex gap-4 mx-9 md:gap-8">
             <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-            <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+            <li className='cursor-pointer hover:font-bold transition-all'>Tasks</li>
             
         </ul>
     </nav>
